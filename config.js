9e060e2c0123e71933f0794c43759f1f5fdcc979
FrangoBolota <94653018+FrangoBolota@users.noreply.github.com>
2023-06-14 19:43:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyA65cOfd1mTkmm8GwExxbe9gkcDr7eIo4g",
+  authDomain: "ciclista-eletronico-49303.firebaseapp.com",
+  projectId: "ciclista-eletronico-49303",
+  storageBucket: "ciclista-eletronico-49303.appspot.com",
+  messagingSenderId: "963937407982",
+  appId: "1:963937407982:web:52a6ab26a56118e9a234ff"
 };
 
 firebase.initializeApp(firebaseConfig);
